9934c5eb1e5b2029fb3adee8ca19cb32b688548c
Alain Petit <alainpetit21@hotmail.com>
2025-10-23 04:47:45 -0400

    Finish Episode 1

--- 1a_RPG_BattleLogParser/e1a.py
+++ 1a_RPG_BattleLogParser/e1a.py
@@ -5,22 +5,25 @@ def main():
         data = file.readlines()
 
     hp = 250
+    all_monsters = set()
     for line in data:
         words = line.split()
 
-        if words[1].startswith("took"):
+
+        if words[1] == "took":
             hp -= int(words[2])
             print(hp)
-        elif words[1].startswith("hit"):
-            ...
-        elif words[1].startswith("takes"):
+        elif words[1] == "hit":
+            all_monsters.add(words[3])
+        elif words[1]  == "takes":
             hp += int(words[5])
             hp = min(hp, 250)
             print(hp)
 
     print(f"Final Health after all battles: {hp}")
+    print(f"Fought the following {len(all_monsters)} monsters: \n{all_monsters}")
 
 
 if __name__ == "__main__":
-    print("Hello e1a")
+    print("Hello 1a")
     main()
--- 1b_RPG_LootBagOrganizer/e1b.py
+++ 1b_RPG_LootBagOrganizer/e1b.py
@@ -2,7 +2,43 @@ import re
 
 
 def to_copper(g=0, s=0, c=0) -> int:
-    ...
+    return g*100+s*10+c
+
+
+def to_gsc(c) -> tuple[int, int, int]:
+    g = c // 100
+    c-= g*100
+
+    s = c // 10
+    c-= s*10
+
+    return g, s, c
+
+
+def summarize_all_winnings(loot) -> tuple[int, int, int]:
+    copper_value = 0
+
+    for item in loot:
+        for all_values in loot[item][1]:
+            copper_value += to_copper(all_values[0], all_values[1], all_values[2])
+
+    return to_gsc(copper_value)
+
+
+def summarize_winnings_bestof(loot):
+    copper_value = 0
+
+    for item in loot:
+        best_value = 0
+        for value_gsc in loot[item][1]:
+            value_c = to_copper(value_gsc[0], value_gsc[1], value_gsc[2])
+
+            if value_c > best_value:
+                best_value = value_c
+
+        copper_value += best_value
+
+    return to_gsc(copper_value)
 
 
 def detect_value(list_value):
@@ -15,21 +51,22 @@ def detect_value(list_value):
     copper = re.findall(r'\d+ copper', ", ".join(list_value))
 
     if len(gold) > 0:
-        gold = int(gold.split())
+        gold = int(gold[0].split()[0])
     else:
         gold = 0
 
     if len(silver) > 0:
-        silver = int(silver.split())
+        silver = int(silver[0].split()[0])
     else:
         silver = 0
 
     if len(copper) > 0:
-        copper = int(copper.split())
+        copper = int(copper[0].split()[0])
     else:
         copper = 0
 
-    return (gold)
+    return gold, silver, copper
+
 
 def main():
     with open("./data/loot_log.txt", "r") as file:
@@ -47,14 +84,16 @@ def main():
 
         loot[item][0] += 1
         valueGSC = detect_value(parts[1:])
-        valueInCopper = to_copper(valueGSC)
+        valueInCopper = to_copper(valueGSC[0], valueGSC[1], valueGSC[2])
         loot[item][1].append(valueGSC)
-        loot[item][1].append(valueInCopper)
 
+    total_value = summarize_all_winnings(loot)
+    print(f"The total value of all the loot in Gold, Silver and copper coins is {total_value}")
 
-    print(loot)
+    total_value = summarize_winnings_bestof(loot)
+    print(f"The total value of all the loot in Gold, Silver and copper coins is {total_value}")
 
 
 if __name__ == "__main__":
-    print("Hello e1b")
+    print("Hello 1b")
     main()
